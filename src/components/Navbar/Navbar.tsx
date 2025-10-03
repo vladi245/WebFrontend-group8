d8b75d5b81615ff50f7f8a870abf98eee553069f
BarB256 <bartoszbobkiewicz1taip@gmail.com>
2025-10-03 17:36:30 +0200

    edited how the styles are applied in the navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,16 +1,16 @@
-import './Navbar.css';
-import GetStandingLogo from '../assets/GetStanding.png';
+import style from './Navbar.module.css';
+import GetStandingLogo from '../../assets/GetStanding.png';
 
 const Navbar = () => {
     return (
-        <nav className="navbar">
-            <div className="navbar-left">
-                <a href="/" className="logo">
+        <nav className={style.navbar}>
+            <div className={style['navbar-left']}>
+                <a href="/" className={style.logo}>
                     <img src={GetStandingLogo} alt="GetStanding Logo" />
                 </a>
             </div>
-            <div className="navbar-center">
-                <ul className="nav-links">
+            <div className={style['navbar-center']}>
+                <ul className={style['nav-links']}>
                     <li><a href="/">Home</a></li>
                     <li><a href="/dashboard">Dashboard</a></li> {/*here would have to add the dropdown in the future */}
                     <li><a href="/community">Community</a></li>
@@ -19,11 +19,11 @@ const Navbar = () => {
 
                 </ul>
             </div>
-            <div className="navbar-right">
-                <a href="/login" className="login-button">
+            <div className={style['navbar-right']}>
+                <a href="/login" className={style['login-button']}>
                     Login
                 </a>
-                <a href="/sign-up" className="signup-button">
+                <a href="/sign-up" className={style['signup-button']}>
                     Sign Up
                 </a>
             </div>
