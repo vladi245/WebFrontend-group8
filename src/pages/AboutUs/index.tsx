082b0f74fe87287644401ccbb2afb962efd88142
0xZarok <vlad.ionita.vi@gmail.com>
2025-11-03 11:46:45 +0100

    fixed email href

--- src/pages/AboutUs/index.tsx
+++ src/pages/AboutUs/index.tsx
@@ -35,7 +35,7 @@ export default function AboutUs() {
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className={styles['contact-icon']}>
                             <path fill="currentColor" d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2m0 4l-8 5l-8-5V6l8 5l8-5z" />
                         </svg>
-                        <a href="mailto:contact@getstanding.com" className={styles['contact-link']}>
+                        <a href="mailto:vibar24@student.sdu.dk" className={styles['contact-link']}>
                             vibar24@student.sdu.dk
                         </a>
                     </div>
