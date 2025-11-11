72e19d455c0df7c6583da77c50172b4f45bd04fd
BarB256 <bartoszbobkiewicz1taip@gmail.com>
2025-11-11 19:29:50 +0100

    feat: wipe dashboard link from nav bar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -12,7 +12,6 @@ const Navbar = () => {
             <div className={style['navbar-center']}>
                 <ul className={style['nav-links']}>
                     <li><a href="/">Home</a></li>
-                    <li><a href="/dashboard">Dashboard</a></li> {/*here would have to add the dropdown in the future */}
                     <li><a href="/about">About Us</a></li>
 
                 </ul>
