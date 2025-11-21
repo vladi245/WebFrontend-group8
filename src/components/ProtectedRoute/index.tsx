a09c78ebbb00e728b415fd12bc4d0dea3d925532
0xZarok <vlad.ionita.vi@gmail.com>
2025-11-21 16:44:27 +0100

    fix

--- src/components/ProtectedRoute/index.tsx
+++ src/components/ProtectedRoute/index.tsx
@@ -2,18 +2,18 @@ import React from 'react'
 import { Navigate, useLocation } from 'react-router-dom'
 
 interface Props {
-  children: React.ReactElement
+    children: React.ReactElement
 }
 
 const ProtectedRoute: React.FC<Props> = ({ children }) => {
-  const location = useLocation()
-  const token = typeof window !== 'undefined' ? localStorage.getItem('token') : null
+    const location = useLocation()
+    const token = typeof window !== 'undefined' ? localStorage.getItem('token') : null
 
-  if (!token) {
-    return <Navigate to="/login" replace state={{ from: location }} />
-  }
+    if (!token) {
+        return <Navigate to="/login" replace state={{ from: location }} />
+    }
 
-  return children
+    return children
 }
 
 export default ProtectedRoute
